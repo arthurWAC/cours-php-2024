eca38f855275f3dc1fa38ac59756393ec9c459e2
Tainix <bonjour@tainix.fr>
2024-01-30 17:03:20 +0100

    Début résolution SAC_1

---
+++ tainix/SAC_1.php
@@ -0,0 +1,20 @@
+<?php
+//== NE PAS TOUCHER
+$sac = 972;
+$objets = [67,85,34,18,53,88,68,61,17,86,19,31,79,33,58,55,36,25,43,82,11,32,40,16,42,38,59,30,13,12,71,41,26,66,72,49,57,22,23,78,35,87,76,28,37,39,70,29,54,47];
+//== NE PAS TOUCHER
+
+// 0. Initialisation
+$sacAventurier = 0;
+
+// 1. Les 10 + lourds
+// 1.1 Trier et essayer de les mettre dans le sac
+
+
+// 2. Les 10 + légers
+// 2.1 Trier et essayer de les mettre dans le sac
+
+function peutRentrerDansLeSac(int $poidsObjet, int $remplissageActuelDuSac, int $remplissageMaxDuSac): bool
+{
+    
+}
